38f0d0bbf7237d7e85c6523de2d4d6abe627b3d7
Richo <richo2192000@gmail.com>
2015-11-28 16:42:09 +0800

    Basic testing.

---
+++ spec/app_spec.rb
@@ -0,0 +1,18 @@
+require_relative 'spec_helper'
+require 'json'
+
+describe 'Bueze UI Testing' do
+  before do
+    unless @browser
+      @headless = Headless.new
+      @browser = Watir::Browser.new
+    end
+    @browser.goto 'localhost:9292'
+  end
+
+  describe 'Visiting the home page' do
+    it 'finds the title' do
+      @browser.title.must_equal 'Bueze'
+    end
+  end
+end
