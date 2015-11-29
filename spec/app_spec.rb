e3b935ac7afaee33b75bd6b0154ce8cb05fb5a7e
crazyplum <crazyplum24@gmail.com>
2015-11-30 00:25:33 +0800

    user page test finished

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -23,5 +23,18 @@ describe 'Bueze UI Testing' do
       @browser.url.must_match %r{http.*/user}
       @browser.text_field(name: 'user_id').exists?.must_equal true
     end
+
+    it 'can search the user with user id' do
+      @browser.link(text: 'Find User').click
+
+      @browser.text_field(name: 'user_id').set('12522728')
+      @browser.button(id: 'home-form-submit').click
+      
+      @browser.url.must_match %r{http.*/user/12522728}
+      @browser.h3.text.must_equal '12522728'
+      @browser.table(class: 'table').exist?.must_equal true
+      @browser.table(class: 'table').rows[0][0].text.must_equal '#'
+      @browser.table(class: 'table').rows[0][1].text.must_equal 'Title'
+    end
   end
 end
