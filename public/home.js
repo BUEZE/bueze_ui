515e760b51094bb16f07339f2fc6bad43d7d98b9
Richo <richo2192000@gmail.com>
2016-01-09 17:54:33 +0800

    Add click events.

--- public/home.js
+++ public/home.js
@@ -1,11 +1,28 @@
 window.onload = function () {
+
   $('#ranking_vis').highcharts({
     yAxis: {
       reversed: true
     }
   });
+
   $.get( "/ranking_chart")
     .done(function( data ) {
       new Chartkick.LineChart("ranking_vis", JSON.parse(data), {library: {yAxis: {reversed: true, min: 0, max: 10}}});
     });
+
+  $('.books').on('click', function(){
+    if($( this ).hasClass('success'))
+      return;
+    $('.books').each(function() {
+      $( this ).removeClass('success');
+    });
+    $( this ).addClass('success');
+
+    $.get( "/ranking_chart")
+      .done(function( data ) {
+        new Chartkick.LineChart("ranking_vis", JSON.parse(data), {library: {yAxis: {reversed: true, min: 0, max: 10}}});
+      });
+  });
+
 }
