84fc4ed4f0b22a86de32376db7d435f28f46029c
Hiraku <dev@hiraku.tw>
2016-01-11 00:36:00 +0800

    hover

--- public/home.js
+++ public/home.js
@@ -14,3 +14,24 @@ function alignNavbar() {
   $("a.navbar-brand").css('margin-left', navPadding + 'px');
   $("#booksearch-form").css('margin-right', navPadding + 'px');
 }
+
+function hoverChartIcon(element) {
+  element.setAttribute('src', '../svg/line_chart_hover.svg');
+}
+function unhoverChartIcon(element) {
+  element.setAttribute('src', '../svg/line_chart.svg');
+}
+
+function hoverSearchIcon(element) {
+  element.setAttribute('src', '../svg/search_hover.svg');
+}
+function unhoverSearchIcon(element) {
+  element.setAttribute('src', '../svg/search.svg');
+}
+
+function hoverPriceIcon(element) {
+  element.setAttribute('src', '../svg/price_hover.svg');
+}
+function unhoverPriceIcon(element) {
+  element.setAttribute('src', '../svg/price.svg');
+}
